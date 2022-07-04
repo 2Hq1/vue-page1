b3f0e52daec6a3a195d4ac3b9c5c284656e5fd47
“2Hq1” <274895084@qq.com>
2022-07-05 00:41:21 +0800

    cu4

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
+/*
+ * @Author: “2Hq1” 274895084@qq.com
+ * @Date: 2022-07-04 21:53:27
+ * @LastEditors: “2Hq1” 274895084@qq.com
+ * @LastEditTime: 2022-07-05 00:39:07
+ * @FilePath: \two-vue-app\vue.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true
+    publicpath: '/vue-page1/'
 })
